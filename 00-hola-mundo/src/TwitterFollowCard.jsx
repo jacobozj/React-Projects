02f4994d5f9bbc5c87a67b7336623ae896c3e631
Jacobo Zuluaga <jacobozuluaga80@gmail.com>
2024-02-27 22:22:42 -0500

    twitter cards implementation

---
+++ 00-hola-mundo/src/TwitterFollowCard.jsx
@@ -0,0 +1,36 @@
+//entre llaves evalua expresiones {aqui}
+import {useState} from 'react'
+export function TwitterFollowCard ( {userName, name}) {
+    const [isFollowing, setIsFollowing] = useState(false) // estado incial, estado final y estado default
+    
+
+
+
+    const text = isFollowing ? 'Siguiendo' : 'Seguir'
+    const buttonClassName = isFollowing ? 'tw-followCard-button is-following' : 'tw-followCard-button'
+    
+    const handleClick = () =>{ //cambia el componente de seguir a dejar de seguir y vice versa
+        setIsFollowing(!isFollowing)
+    }
+    
+    return (
+        <article className='tw-followCard'>
+            <header className='tw-followCard-header'>
+                <img 
+                className='tw-followCard-avatar'
+                src={`https://unavatar.io/${userName}`} 
+                alt="Avatar"/>
+                <div className='tw-followCard-info'>
+                    <strong>{name}</strong>
+                    <span className='tw-followCard-infoUserName'>@{userName}</span>
+                </div>
+            </header>
+            <aside>
+                <button className={buttonClassName} onClick={handleClick}>
+                    <span className='tw-followCard-text'>{text}</span>
+                    <span className='tw-followCard-stopFollow'>Dejar de seguir</span>
+                </button>
+            </aside>
+        </article>
+    )
+}
